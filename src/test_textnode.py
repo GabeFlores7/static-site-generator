956e716eb147cb65cd621ff69d3a6780d2f66e11
GabeFlores7 <gabe7.flores@gmail.com>
2025-09-19 20:34:37 -0500

    Creat text_node_to_html_node function with respective tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,8 +1,9 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
+# 6 Tests for 'TextType' class
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
@@ -32,6 +33,35 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("", TextType.CODE, url= "https://www.boot.dev")
         node2 = TextNode("", TextType.CODE, url= "https://www.boot.dev/")
         self.assertEqual(node, node2)
+# 4 Tests for 'text_node_to_html_node' fxn
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+    
+    def test_link(self):
+        node = TextNode("This is a link", TextType.LINK, "https://open.spotify.com/")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link")
+        self.assertEqual(html_node.props, {"href":"https://open.spotify.com/"})
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
-# Code used as an intermediate representation of inline text
 from enum import Enum
+from htmlnode import LeafNode
 # types of text nodes covered by the script
 class TextType(Enum):
     TEXT = "text"
@@ -8,7 +8,7 @@ class TextType(Enum):
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
-
+# Class used as an intermediate representation of inline text
 class TextNode:
     def __init__(self, text, text_type, url= None):
         self.text = text
@@ -24,3 +24,20 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+# Function to convert text_node to a LeafNode of HTML_node class
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag= None, value= text_node.text, props= None)
+        case TextType.BOLD:
+            return LeafNode(tag= 'b', value= text_node.text, props= None)
+        case TextType.ITALIC:
+            return LeafNode(tag= 'i', value= text_node.text, props= None)
+        case TextType.CODE:
+            return LeafNode(tag= 'c', value= text_node.text, props= None)
+        case TextType.LINK:
+            return LeafNode(tag= 'a', value= text_node.text, props= {"href":text_node.url})
+        case TextType.IMAGE:
+            return LeafNode(tag= 'img', value= '', props= {"src":text_node.url, "alt":text_node.text})
+        case _:
+            raise Exception("invalid text: 'text type'")
