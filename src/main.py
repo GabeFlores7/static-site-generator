501cb69391474e94cede04088cf51a449bc9aaa4
GabeFlores7 <gabe7.flores@gmail.com>
2025-09-23 19:18:16 -0500

    Modify 'main.py' and 'page_generation.py' functions to deploy on GitHub repo

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import shutil
 from block_markdown import extract_title
@@ -5,22 +6,27 @@ from copystatic import copy_static_to
 from page_generation import generate_pages_recursive
         
 def main():
+    # Configure root
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = '/'
     # 'copy_static_to' inputs
     static_path = "./static"
-    public_path = "./public"
+    webpage_path = "./docs"
     # 'generate_pate' inputs
     content_path = "./content"
     template_path = "./template.html"
 
-    print(f"Deleting {public_path.replace('./', '')} directory...")
+    print(f"Deleting {webpage_path.replace('./', '')} directory...")
     # Delete everything in dst folder upon intial start of function
-    if os.path.exists(public_path):
-        shutil.rmtree(public_path)
-    # Recursively copy static files to public directory 
-    print(f"Copying {static_path.replace('./', '')} files to {public_path.replace('./', '')} directory...")
-    copy_static_to(static_path, public_path)
-    # Generate pages from 'content' using 'template.html' and write to 'public'
-    generate_pages_recursive(content_path, template_path, public_path)
+    if os.path.exists(webpage_path):
+        shutil.rmtree(webpage_path_path)
+    # Recursively copy static files to docs directory 
+    print(f"Copying {static_path.replace('./', '')} files to {webpage_path.replace('./', '')} directory...")
+    copy_static_to(static_path, webpage_path)
+    # Generate pages from 'content' using 'template.html' and write to 'docs'
+    generate_pages_recursive(content_path, template_path, webpage_path, basepath)
 
 if __name__ == "__main__":
     main()
--- src/page_generation.py
+++ src/page_generation.py
@@ -3,7 +3,7 @@ from markdown_to_html import markdown_to_html_node
 from block_markdown import extract_title
 
 # Function to generate and serve webpage
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     # Function initialization/ read files
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, mode= 'r') as f:
@@ -17,6 +17,8 @@ def generate_page(from_path, template_path, dest_path):
     # Modify template content
     webpage = template_file.replace("{{ Content }}", html_content)
     webpage = webpage.replace("{{ Title }}", html_title)
+    webpage = webpage.replace("href=\"/", f"href=\"{basepath}")
+    webpage = webpage.replace("src=\"/", f"src=\"{basepath}")
 
     # Make 'destination directory' if non-existent
     dest_dir = os.path.dirname(dest_path)
@@ -26,7 +28,7 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, mode= 'w') as f:
         f.write(webpage)
     
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     # Make dest path if nonexistent
     if not os.path.exists(dest_dir_path):
         os.mkdir(dest_dir_path)
@@ -38,6 +40,6 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         new_dst_path = os.path.join(dest_dir_path, entry)
         if os.path.isfile(content_entry_path):
             new_dst_path = new_dst_path.replace('.md', '.html') # correct file extension
-            generate_page(content_entry_path, template_path, new_dst_path)
+            generate_page(content_entry_path, template_path, new_dst_path, basepath)
         else:
-            generate_pages_recursive(content_entry_path, template_path, new_dst_path)
+            generate_pages_recursive(content_entry_path, template_path, new_dst_path, basepath)
